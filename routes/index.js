83380517c8f6dd61e08efcddf74de0ade00ca84c
Richard Adjogah <richard.adjogah@gmail.com>
2017-02-08 19:48:53 -0500

    Add basic demo

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ exports.routes = function(app) {
 	app.get('/', exports.blood);
 	app.get('/blood', exports.blood);
 	app.get('/visualizer', exports.visualizer);
+	app.get('/demo', exports.demo);
 	app.get('/static', exports.blood);
 	app.get('/track/:track', exports.track)
 };
@@ -17,6 +18,11 @@ exports.blood = function(req, res) {
 exports.visualizer = function(req, res) {
 	res.render('bloodVis', {});
 };
+
+exports.demo = function(req, res) {
+	res.render('demo', {});
+};
+
 
 exports.track = function(req, res) {
 	
