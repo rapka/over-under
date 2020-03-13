5327e2c520e412712ccb7cdbf2d7b6d3a0ab81ad
Richard Adjogah <richard@webs.com>
2020-03-13 01:11:13 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import Track from './Track';
 import './App.css';
 
 const SAMPLE_COUNTS = [8, 9, 7];
+const DURATIONS = [84, 145, 96, 124, 80, 46, 83, 128, 150, 152, 119, 137, 147, 135, 129, 123, 192];
+// const DURATIONS = [10, 10, 96, 124, 80, 46, 83, 128, 150, 152, 119, 137, 147, 135, 129, 123, 192];
 
 function App() {
   let tracks = [];
 
   _.each(SAMPLE_COUNTS, (count, i) => {
-   tracks.push(<Track track={i + 1} sampleCount={count} />)
+   tracks.push((
+    <Track
+      key={i}
+      track={i + 1}
+      sampleCount={count}
+      duration={DURATIONS[i]}
+      offset={_.sum(DURATIONS.slice(0, i))}
+    />
+  ))
   });
 
   return (
--- src/Track.js
+++ src/Track.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import _ from 'lodash'
 
 import Cover from './Cover';
 import './Track.css';
 
-function Track(props) {
-  let covers = [];
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { visible: false };
+  }
 
-  _.times(props.sampleCount, n => {
-    covers.push(
-      <Cover
-        track={props.track}
-        sample={n + 1}
-        samples={props.sampleCount}
-        backward={props.track % 2 === 0}
-      />
-    )
-  });
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ visible: true });
+    }, (this.props.offset - 2) * 1000);
+    setTimeout(() => {
+      this.setState({ visible: false })
+    }, ( this.props.offset + this.props.duration + 3 ) * 1000);
+  }
 
-  return (
-    <div
-      className="track"
-      style={{
-        transform: `translateY(${(props.track - 1) * 100}px)`,
-        animationDelay: `${props.offset}s`,
-        animationDuration: `${props.duration}s`
-      }}
-    >
-      {covers}
-    </div>
-  );
+
+  render() {
+    let covers = [];
+
+    _.times(this.props.sampleCount, n => {
+      covers.push(
+        <Cover
+          key={n}
+          track={this.props.track}
+          sample={n + 1}
+          samples={this.props.sampleCount}
+          backward={n % 2 === 0}
+        />
+      )
+    });
+
+    return this.state.visible ? (
+      <div
+        className="track"
+        style={{
+          animationDelay: `${this.props.offset}s`,
+          animationDuration: `${this.props.duration + 2}s`,
+          //display: visible ? 'block' : 'none',
+        }}
+      >
+        {covers}
+      </div>
+    ) : null;
+  }
+
 }
 
 export default Track;
